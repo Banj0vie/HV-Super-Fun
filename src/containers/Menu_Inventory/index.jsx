1b1710d989967578a1a84e18665ad50b6e49b43c
auditor0209 <samanthaglory2@gmail.com>
2025-09-23 00:20:57 -0400

    Update index.jsx

--- src/containers/Menu_Inventory/index.jsx
+++ src/containers/Menu_Inventory/index.jsx
@@ -2,11 +2,7 @@ import React, { useEffect, useState } from "react";
 import "./style.css";
 import BaseDialog from "../_BaseDialog";
 import {
-  ID_CHEST_ITEMS,
   ID_INVENTORY_MENUS,
-  ID_LOOTS,
-  ID_PRODUCE_ITEMS,
-  ID_SEEDS,
 } from "../../constants/app_ids";
 import BaseButton from "../../components/buttons/BaseButton";
 import ItemSmallView from "../../components/boxes/ItemViewSmall";
@@ -20,151 +16,9 @@ const menus = [
   { id: ID_INVENTORY_MENUS.ITEMS, label: "Items" },
 ];
 
-const TESTING_SEEDS = [
-  {
-    id: ID_SEEDS.F_POTATO,
-    count: 2,
-  },
-  {
-    id: ID_SEEDS.F_CABBAGE,
-    count: 2,
-  },
-  {
-    id: ID_SEEDS.F_LETTUCE,
-    count: 3,
-  },
-  {
-    id: ID_SEEDS.F_ONION,
-    count: 1,
-  },
-  {
-    id: ID_SEEDS.F_POTATO,
-    count: 2,
-  },
-  {
-    id: ID_SEEDS.F_RADISH,
-    count: 2,
-  },
-];
-
-const TESTIING_PRODUCE = [
-  {
-    id: ID_PRODUCE_ITEMS.BANANA,
-    count: 2,
-  },
-  {
-    id: ID_PRODUCE_ITEMS.ARTICHOKE,
-    count: 4,
-  },
-  {
-    id: ID_PRODUCE_ITEMS.AVOCADO,
-    count: 1,
-  },
-  {
-    id: ID_PRODUCE_ITEMS.BERRY,
-    count: 5,
-  },
-  {
-    id: ID_PRODUCE_ITEMS.BLUEBERRY,
-    count: 6,
-  },
-  {
-    id: ID_PRODUCE_ITEMS.BROCCOLI,
-    count: 7,
-  },
-  {
-    id: ID_PRODUCE_ITEMS.CABBAGE,
-    count: 8,
-  },
-  {
-    id: ID_PRODUCE_ITEMS.CARROT,
-    count: 9,
-  },
-  {
-    id: ID_PRODUCE_ITEMS.CAULIFLOWER,
-    count: 1,
-  },
-  {
-    id: ID_PRODUCE_ITEMS.CAULIFLOWER,
-    count: 5,
-  },
-  {
-    id: ID_PRODUCE_ITEMS.CELERY,
-    count: 4,
-  },
-  {
-    id: ID_PRODUCE_ITEMS.CHILI,
-    count: 4,
-  },
-  {
-    id: ID_PRODUCE_ITEMS.CORN,
-    count: 8,
-  },
-  {
-    id: ID_PRODUCE_ITEMS.DRAGONFRUIT,
-    count: 5,
-  },
-  {
-    id: ID_PRODUCE_ITEMS.FIG,
-    count: 2,
-  },
-  {
-    id: ID_PRODUCE_ITEMS.GRAPES,
-    count: 5,
-  },
-  {
-    id: ID_PRODUCE_ITEMS.LAVENDER,
-    count: 6,
-  },
-  {
-    id: ID_PRODUCE_ITEMS.LETTUCE,
-    count: 8,
-  },
-  {
-    id: ID_PRODUCE_ITEMS.MANGO,
-    count: 5,
-  },
-];
-
-const TESTING_FISHES = [
-  {
-    id: ID_LOOTS.CATFISH,
-    count: 1,
-  },
-  {
-    id: ID_LOOTS.CATFISH,
-    count: 3,
-  },
-  {
-    id: ID_LOOTS.CATFISH,
-    count: 6,
-  },
-  {
-    id: ID_LOOTS.CATFISH,
-    count: 2,
-  },
-  {
-    id: ID_LOOTS.CATFISH,
-    count: 3,
-  },
-];
-
-const TESTING_ITEMS = [
-  {
-    id: ID_CHEST_ITEMS.CHEST_SILVER,
-    count: 2,
-    usable: false,
-  },
-  {
-    id: ID_CHEST_ITEMS.CHEST_GOLD,
-    count: 3,
-    usable: true,
-  },
-];
-
 const InventoryDialog = ({ onClose }) => {
   const [selectedMenu, setSelectedMenu] = useState(ID_INVENTORY_MENUS.SEEDS);
-  const { all: allItems, loading, error } = useItems();
+  const { all: allItems } = useItems();
   console.log("allItems", allItems);
   const [list, setList] = useState([]);
 
